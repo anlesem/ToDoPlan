74524bf412317be6d8b7c54c82f76a1db72e96f0
anlesem <anlesem@mail.ru>
2021-10-04 13:34:31 +0300

    v0.1_Структура.Адаптив.

---
+++ js/scritps.js
@@ -0,0 +1,40 @@
+function start(event) {
+	initialPoint = event.changedTouches[0];
+}
+
+function end(event) {
+	finalPoint = event.changedTouches[0];
+	var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+	var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+	if (xAbs > 30 || yAbs > 30) {																									// Диапазон смещения. Данный весьма мал, но сбоев не обнаружено
+		if (xAbs > yAbs) {
+			console.log("1");
+			if (finalPoint.pageX < initialPoint.pageX) {
+				console.log("2");
+				if (asideTable.checked && window.innerWidth < mobileWidth) asidePanel.checked = true; 			// Движение влево  
+			}
+			else if (asidePanel.checked && window.innerWidth < mobileWidth) asideTable.checked = true;;		// Движение вправо
+		}
+	}
+}
+
+
+//*---------------------------------------------- ПЕРЕМЕННЫЕ
+
+var initialPoint;				// начало движения
+var finalPoint;				// конец движения
+
+var mobileWidth = 800;
+
+let asideTable = document.getElementById('aside-table');
+let asidePanel = document.getElementById('aside-panel');
+
+console.log(asideTable.checked);
+console.log(asidePanel.checked);
+
+
+//*---------------------------------------------- ОТСЛЕЖИВАНИЕ
+
+// Отслеживание. Движения
+document.addEventListener('touchstart', start, false);
+document.addEventListener('touchend', end, false);
