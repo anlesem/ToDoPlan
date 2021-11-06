08fbf895a436bd86229970c6a1368e6901f6b62f
anlesem <anlesem@mail.ru>
2021-11-06 22:19:15 +0300

    table__swap.Переключение столбцов при адаптации

--- js/scritps.js
+++ js/scritps.js
@@ -1,3 +1,4 @@
+// Переключение отображения полей таблицы при малых размерах экрана
 function start(event) {
 	initialPoint = event.changedTouches[0];
 }
@@ -9,13 +10,32 @@ function end(event) {
 	if (xAbs > 30 || yAbs > 30) {																									// Диапазон смещения. Данный весьма мал, но сбоев не обнаружено
 		if (xAbs > yAbs) {
 			if (finalPoint.pageX < initialPoint.pageX) {
-				if (asideTable.checked && window.innerWidth < mobileWidth) asidePanel.checked = true; 			// Движение влево  
+				if (window.innerWidth < tableSwapWidth) slide('next'); 			// Движение влево  
 			}
-			else if (asidePanel.checked && window.innerWidth < mobileWidth) asideTable.checked = true;;		// Движение вправо
+			else if (window.innerWidth < tableSwapWidth) slide('prev');			// Движение вправо
 		}
 	}
 }
 
+function slide(n) {
+	for (let i = 0; i < tableSwapList.length; i++) {
+		if (tableSwapList[i].checked) {									// Определение актуальной позиции input.checked 
+			if (n == 'next') {												// Для следующего
+				if ((i + 1) == tableSwapList.length) return;			// Проверка последний ли?		
+				else {
+					tableSwapList[i + 1].checked = true;				// Переключение позиции input.checked
+					return;														// Завершение именно функции, а не цикла
+				}
+			} else if (i == 0) return;									// Для предыдущего. Проверка первый ли?
+				else {
+					tableSwapList[i - 1].checked = true;				// Переключение позиции input.checked
+				return;															// Завершение именно функции, а не цикла
+			}
+		}
+	}
+}
+
+// Входная группа
 function enterLabel(id) {
 	var e = document.querySelector(id).value.trim().length;
 	if (e) document.querySelector(id + '+label').classList.add('fill');
@@ -25,13 +45,17 @@ function enterLabel(id) {
 
 //*---------------------------------------------- ПЕРЕМЕННЫЕ
 
+// Переключение отображения полей таблицы при малых размерах экрана
 var initialPoint;				// начало движения
 var finalPoint;				// конец движения
 
-var mobileWidth = 800;
+let tableSwap_name = document.getElementById('table__swap-name');
+let tableSwap_set = document.getElementById('table__swap-set');
+let tableSwap_note = document.getElementById('table__swap-note');
+let tableSwap_money = document.getElementById('table__swap-money');
 
-let asideTable = document.getElementById('aside-table');
-let asidePanel = document.getElementById('aside-panel');
+var tableSwapWidth = 1100;
+var tableSwapList = [tableSwap_name, tableSwap_set, tableSwap_note, tableSwap_money]
 
 // Входная группа
 let passEnterLogin = document.getElementById('pass__enter-login');
@@ -40,6 +64,11 @@ let passRegistrationLogin = document.getElementById('pass__registration-login');
 let passRegistrationEmail = document.getElementById('pass__registration-email');
 let passRegistrationPassword = document.getElementById('pass__registration-password');
 
+// Отключение финансового блока
+let moneyOff = document.getElementById('money-off');
+let moneyPIN = document.getElementById('money-pin');
+let money = document.getElementById('money');
+
 
 //*---------------------------------------------- ОТСЛЕЖИВАНИЕ
 
@@ -47,17 +76,25 @@ let passRegistrationPassword = document.getElementById('pass__registration-passw
 document.addEventListener('touchstart', start, false);
 document.addEventListener('touchend', end, false);
 
+// Входная группа
 passEnterLogin.addEventListener('focusout', () => enterLabel('#pass__enter-login'));
 passEnterPassword.addEventListener('focusout', () => enterLabel('#pass__enter-password'));
 passRegistrationLogin.addEventListener('focusout', () => enterLabel('#pass__registration-login'));
 passRegistrationEmail.addEventListener('focusout', () => enterLabel('#pass__registration-email'));
 passRegistrationPassword.addEventListener('focusout', () => enterLabel('#pass__registration-password'));
 
-// passEnterLogin.addEventListener('mouseout', () => enterLabel('#pass__enter-login'));
-// passEnterPassword.addEventListener('mouseout', () => enterLabel('#pass__enter-password'));
-// passRegistrationLogin.addEventListener('mouseout', () => enterLabel('#pass__registration-login'));
-// passRegistrationEmail.addEventListener('mouseout', () => enterLabel('#pass__registration-email'));
-// passRegistrationPassword.addEventListener('mouseout', () => enterLabel('#pass__registration-password'));
-
+// Отключение финансового блока
+moneyOff.addEventListener('click', function () {
+	if (moneyOff.checked) {
+		money.checked = false;
+		moneyPIN.disabled = false;
+		tableSwap_name.checked = true;
+		tableSwapList.splice(3, 1);
+	}
+	else {
+		moneyPIN.disabled = true;
+		tableSwapList.push(tableSwap_money);
+	}
+}, false);
 
 
